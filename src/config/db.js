d3a7d264ca3b39b4a1c1344894acf1269466922d
mohamedalswelhi01-a11y <mohamedalswelhi01@gmail.com>
2026-09-25 12:28:36 +0200

    Create db.js

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes('sslmode=require') ? { rejectUnauthorized: false } : false,
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  pool,
+};
